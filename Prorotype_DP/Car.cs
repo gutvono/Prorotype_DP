109e36b31a78ce11496a0e15c0380a63d2f67c60
Gustavo <guvono4@gmail.com>
2024-05-31 10:37:00 -0300

    Add project files.

---
+++ Prorotype_DP/Car.cs
@@ -0,0 +1,37 @@
+﻿using Prorotype_DP;
+
+public class Car : IPrototype
+{
+    public int? Id { get; set; }
+    private string CPF { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    private int Year { get; set; }
+
+    public Car() 
+    {
+        this.CPF = "484.512.608-70";
+        this.Name = "Celta";
+        this.Color = "Prata";
+        this.Year = 2005;
+    }
+
+    public Car(Car car)
+    {
+        CPF = car.CPF;
+        Name = car.Name;
+        Color = car.Color;
+        Year = car.Year;
+    }
+
+    public IPrototype Clone() { return new Car(this); }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}\n" +
+            $"CPF do dono: {CPF}\n" +
+            $"Nome: {Name}\n" +
+            $"Cor: {Color}\n" +
+            $"Ano: {Year}\n";
+    }
+}
---
+++ Prorotype_DP/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prorotype_DP;
+
+Car car = new();
+
+Car novoCarro = new Car
+{
+    CPF = car.CPF,
+    Name = car.Name,
+    Color = car.Color
+};
+
+var clone = car.Clone();
+
+
+Console.WriteLine(car);
+
+Console.WriteLine(clone);
